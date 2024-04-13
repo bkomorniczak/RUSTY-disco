1b9c7866e648dedb8f9ed7426d21288893cea80d
Grzegorz Karkowski <grzegorzkarkowski7@gmail.com>
2024-04-13 05:59:51 +0200

    generate_key_fix

--- src/generate_key.rs
+++ src/generate_key.rs
@@ -1,18 +1,23 @@
-// use rand::Rng;
-// use std::fs::File;
-// use std::io;
-// use std::io::Write;
-//
-// fn generate_key_map() -> io::Result<()> {
-//     let mut file = File::create(DICTIONARY_PATH)?;
-//     for i in 65u8..90 {
-//         let letter = i as char;
-//         let mut rng = rand::thread_rng();
-//         let key: char = rng.gen_range('A'..='Z');
-//         let result = format!("{}\t{}\n", letter, key);
-//         write!(file, "{}", result)?;
-//     }
-//     Ok(())
-// }
-//
-// const DICTIONARY_PATH: &'static str = "src/resource/dictionary.txt";
+ use rand::Rng;
+ use std::fs::File;
+ use std::io;
+ use std::io::Write;
+
+fn generate_key_map() -> io::Result<()> {
+    let mut file = File::create(DICTIONARY_PATH)?;
+    let mut used_keys = HashSet::new();
+    let mut rng = rand::thread_rng();
+
+    for i in 65u8..90 {
+        let letter = i as char;
+        let key: char = rng.gen_range('A'..='Z');
+        while(used_keys.contains(&key)) {
+            used_keys.insert(key);
+        }
+        let result = format!("{}\t{}\n", letter, key);
+        write!(file, "{}", result)?;
+    }
+    Ok(())
+}
+
+ const DICTIONARY_PATH: &'static str = "src/resource/dictionary.txt";
